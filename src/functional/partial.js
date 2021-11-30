cd29f8babe8aec6e29b15c05dd45e68c7c91570a
thornsh <1183643226@qq.com>
2021-11-30 12:06:48 +0800

    feat: :zap: add curry of proxy

--- src/functional/partial.js
+++ src/functional/partial.js
@@ -4,4 +4,38 @@ function partial(fn, ...preArgs) {
   }
 }
 
-module.exports = partial
+function curry(fn) {
+  const fnArgsLength = fn.length
+  return (function nextCurried(preArgs) {
+    return function (currArg) {
+      const currentPreArgs = preArgs.concat([currArg])
+      if (currentPreArgs.length >= fnArgsLength) {
+        return fn(...currentPreArgs)
+      } else {
+        return nextCurried(currentPreArgs)
+      }
+    }
+  })([])
+}
+
+function curryProxy(fn) {
+  const fnArgsLength = fn.length
+  return (function nextCurried(preArgs) {
+    const proxyObj = new Proxy(fn, {
+      apply(_target, context, args) {
+        const currentPreArgs = preArgs.concat([...args])
+        if (currentPreArgs.length >= fnArgsLength) {
+          return fn.apply(context, currentPreArgs)
+        }
+        return nextCurried(currentPreArgs)
+      }
+    })
+    return proxyObj
+  })([])
+}
+
+module.exports = {
+  partial,
+  curry,
+  curryProxy
+}
--- test/functional/partial.test.js
+++ test/functional/partial.test.js
@@ -1,9 +1,19 @@
-const partial = require('../../src/functional/partial')
+const { partial, curry, curryProxy } = require('../../src/functional/partial')
 const expect = require('chai').expect
 
 describe('partial函数测试', function () {
   const add = (a, b) => a + b
   it('1 加 1 应该等于 2', function () {
     expect(partial(add, 1)(1)).to.be.equal(add(1, 1))
+  })
+})
+
+describe('curry函数测试', function () {
+  const add = (a, b, c) => a + b + c
+  it('add(1,2,3) = 6', function () {
+    expect(curry(add)(1)(2)(3)).to.be.equal(add(1, 2, 3))
+  })
+  it('add(1,2,3) = 6', function () {
+    expect(curryProxy(add)(1)(2)(3)).to.be.equal(add(1, 2, 3))
   })
 })
